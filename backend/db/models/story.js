feb24958f465a0e77e77081f8fef5b3079a6e0f4
Jacob Grooms <grooms.jacob@gmail.com>
2021-03-01 17:20:38 -0500

    generating models

---
+++ backend/db/models/story.js
@@ -0,0 +1,44 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+	const Story = sequelize.define(
+		"Story",
+		{
+			title: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			subtitle: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			content: {
+				type: DataTypes.TEXT,
+				allowNull: false,
+			},
+			authorId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				references: { model: "Users" },
+			},
+			image: {
+				type: DataTypes.TEXT,
+				allowNull: false,
+			},
+		},
+		{}
+	);
+	Story.associate = function (models) {
+		Story.belongsTo(models.User, { foreignKey: "authorId" });
+		Story.hasMany(models.Like, {
+			foreignKey: "storyId",
+			onDelete: "cascade",
+			hooks: true,
+		});
+		Story.hasMany(models.Comment, {
+			foreignKey: "storyId",
+			onDelete: "cascade",
+			hooks: true,
+		});
+	};
+	return Story;
+};
