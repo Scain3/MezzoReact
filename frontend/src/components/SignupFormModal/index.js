84eeb192e3ec37ef7737dd1867c7bca42a489535
Jacob Grooms <grooms.jacob@gmail.com>
2021-03-04 22:34:36 -0500

    signup modal now switches forms, starting styling

--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
 import { Modal } from "../../context/Modal";
 import SignupForm from "./SignupForm";
+import LoginForm from "../LoginFormModal/LoginForm";
 
 function LoginFormModal() {
 	const [showModal, setShowModal] = useState(false);
+	const [signup, setSignup] = useState(true);
 
 	return (
 		<>
-			<button className="login-modal-button" onClick={() => setShowModal(true)}>Sign Up</button>
+			<button className="login-modal-button" onClick={() => setShowModal(true)}>
+				Sign Up
+			</button>
 			{showModal && (
 				<Modal onClose={() => setShowModal(false)}>
-					<SignupForm />
+					{signup ? (
+						<SignupForm setSignup={setSignup} />
+					) : (
+						<LoginForm setSignup={setSignup} />
+					)}
 				</Modal>
 			)}
 		</>
