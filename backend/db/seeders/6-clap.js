32b92a697b8378affc6f1ac16c27a730ba180175
Jacob Grooms <grooms.jacob@gmail.com>
2021-03-10 13:02:42 -0500

    creating seeder data for claps

---
+++ backend/db/seeders/6-clap.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.bulkInsert("Claps", [
+			{ userId: 1, storyId: 1, createdAt: new Date(), updatedAt: new Date() },
+			{ userId: 1, storyId: 2, createdAt: new Date(), updatedAt: new Date() },
+			{ userId: 1, storyId: 2, createdAt: new Date(), updatedAt: new Date() },
+			{ userId: 1, storyId: 3, createdAt: new Date(), updatedAt: new Date() },
+			{ userId: 1, storyId: 3, createdAt: new Date(), updatedAt: new Date() },
+			{ userId: 1, storyId: 3, createdAt: new Date(), updatedAt: new Date() },
+			{ userId: 1, storyId: 4, createdAt: new Date(), updatedAt: new Date() },
+			{ userId: 1, storyId: 4, createdAt: new Date(), updatedAt: new Date() },
+			{ userId: 1, storyId: 4, createdAt: new Date(), updatedAt: new Date() },
+			{ userId: 1, storyId: 4, createdAt: new Date(), updatedAt: new Date() },
+			{ userId: 1, storyId: 5, createdAt: new Date(), updatedAt: new Date() },
+			{ userId: 1, storyId: 5, createdAt: new Date(), updatedAt: new Date() },
+			{ userId: 1, storyId: 5, createdAt: new Date(), updatedAt: new Date() },
+			{ userId: 1, storyId: 5, createdAt: new Date(), updatedAt: new Date() },
+			{ userId: 1, storyId: 5, createdAt: new Date(), updatedAt: new Date() },
+		]);
+	},
+
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.bulkDelete("Claps", null, {});
+	},
+};
