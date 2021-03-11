6f4926e0e7a20f16016634c27127be89f64eb00c
Safiya Cain <say.cain3@gmail.com>
2021-03-11 15:53:44 -0500

    style search bar

--- frontend/src/components/Search/SearchBar.js
+++ frontend/src/components/Search/SearchBar.js
@@ -1,8 +1,16 @@
-
+// import React, {useState} from "react";
+// import {useDispatch} from "react-redux";
+// import {fetchProductInfo} from "../../store/search";
+// import {useHistory} from "react-router-dom";
+import './Search.css';
 
 function SearchBar(){
+    // const [search, setSearch] = useState();
+    // const dispatch = useDispatch();
+    // const history = useHistory();
+
     return(
-        <form>
+        <form className="nav-search__form">
             <input
                  className="nav-search__input"
                  placeholder="Search"
