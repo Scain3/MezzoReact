1ec3076834cc5396eb780bd6c874f44607dc5180
Safiya Cain <say.cain3@gmail.com>
2021-03-11 16:13:56 -0500

    create reducer function for search

---
+++ frontend/src/store/search.js
@@ -0,0 +1,30 @@
+import {fetch} from './csrf.js';
+
+const SEARCH_STORIES = 'SEARCH_STORIES';
+
+const searchStories= (stories) => ({
+    type: SEARCH_STORIES,
+    payload: stories
+})
+
+export const fetchSearchInfo = (term) => async(dispatch) => {
+    const res = await fetch(`/api/search?term=${encodeURIComponent(term)}`)
+
+    const data = await res.json();
+    const story = data.stories;
+
+    dispatch(searchStories(story));
+}
+
+const initialState = {story: null};
+
+const searchReducer = (state= initialState, action) => {
+    switch(action.type){
+        case SEARCH_STORIES:
+            return { ...state, story: action.payload}
+        default:
+            return state;
+    }
+}
+
+export default searchReducer;
