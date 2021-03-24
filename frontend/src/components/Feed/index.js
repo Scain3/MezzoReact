c4756d0b3b62bae84d167cf23ddbc4310a1cd66f
salina kuo <salina.kuo97@gmail.com>
2021-03-24 17:40:22 -0400

    starting feed previews

--- frontend/src/components/Feed/index.js
+++ frontend/src/components/Feed/index.js
@@ -10,9 +10,12 @@ const Feed = () => {
 
     return (
             <>
+                <h1>This is the feed</h1>
                 <div className="feed-container">
-
+                    {/* story preview */}
                 </div>
             </>
     )
 }
+
+export default Feed;
--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,22 +1,27 @@
 import React from "react";
-import TrendingStories from "../TrendingStories";
+import { useSelector } from "react-redux";
 import SignupFormModal from "../SignupFormModal";
+import TrendingStories from "../TrendingStories";
+import Feed from "../Feed";
 import "./index.css";
 
 const HomePage = () => {
-
+	const sessionUser = useSelector(state => state.session.user);
 
 
 	return (
 		<>
-			<div className="intro-container">
-				<div className="intro">
-					<h1 className="heading">Explore new perspectives</h1>
-					<h3 className="sub-heading">Read and share ideas from independent artists, world-class performers, experts, and self-taught musicians from around the globe. Everyone's welcome.</h3>
-					<SignupFormModal />
+			{ !sessionUser ? (
+				<div className="intro-container">
+					<div className="intro">
+						<h1 className="heading">Explore new perspectives</h1>
+						<h3 className="sub-heading">Read and share ideas from independent artists, world-class performers, experts, and self-taught musicians from around the globe. Everyone's welcome.</h3>
+						<SignupFormModal />
+					</div>
 				</div>
-			</div>
+			) : null}
             <TrendingStories />
+			<Feed />
 		</>
 	);
 };
