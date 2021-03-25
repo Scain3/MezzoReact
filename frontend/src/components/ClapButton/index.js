8a0571ed69f39f7d061e137f592ac7dd4d9b59d7
Jacob Grooms <grooms.jacob@gmail.com>
2021-03-24 21:27:20 -0400

    claps button styled and placed

--- frontend/src/components/ClapButton/index.js
+++ frontend/src/components/ClapButton/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+
+import clap from "../../images/clap.svg"
 import "./clapButton.css"
 
 const ClapButton = () => {
     return (
 			<button className="clap-button">
-				<img src="../../images/clap.svg" />
+				<img className="clap-button-icon" src={clap} alt="clap" />
+                <p className="clap-button-clap-count">1k</p>
 			</button>
 		);
 };
