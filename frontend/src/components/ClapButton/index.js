80c8f2a53bc3183142a8190895bc3afab8b8c803
Jacob Grooms <grooms.jacob@gmail.com>
2021-03-24 20:29:25 -0400

    new branch, declaring ClapButton, adding images directory to src

---
+++ frontend/src/components/ClapButton/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./clapButton.css"
+
+const ClapButton = () => {
+    return (
+			<button className="clap-button">
+				<img src="../../images/clap.svg" />
+			</button>
+		);
+};
+
+export default ClapButton;
--- frontend/src/components/StoryPage/LoggedInStory.js
+++ frontend/src/components/StoryPage/LoggedInStory.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import CommentsSection from "../CommentsSection";
+import ClapButton from "../ClapButton";
 
 const LoggedInStory = ({ content, storyId }) => {
 	return (
 		<>
 			<p className="logged-in-story-page-content">{content}</p>
 			<div className="logged-in-story-controls">
+				<ClapButton storyId={storyId} />
 				<CommentsSection storyId={storyId} />
 			</div>
 		</>
