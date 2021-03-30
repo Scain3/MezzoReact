dfae9b06edc10a6624c8a6208d4216570762d2d7
Safiya Cain <say.cain3@gmail.com>
2021-03-30 19:55:36 -0400

    style search landing page

--- frontend/src/components/Search/SearchPage.js
+++ frontend/src/components/Search/SearchPage.js
@@ -1,22 +1,22 @@
 import React from "react";
 import {useSelector} from "react-redux";
 import { NavLink } from "react-router-dom";
-// import {useHistory} from "react-router-dom";
+import {useHistory} from "react-router-dom";
 import './Search.css';
 
 function SearchPage(){
-    // const history = useHistory();
+    const history = useHistory();
 
     const searchInfo = useSelector(state => state.search.story);
     return(
         <div>{
             searchInfo && searchInfo.map((search, index) => (
                 <div className="search-landing__container" >
-                    <div>
-                        <div>{`${search.User.firstName} ${search.User.lastName}`}</div>
-                        <NavLink to={`/story/${search.id}`} className="search-landing__title">{search.title}</NavLink>
-                        <div className="search-landing__subtitle">{search.subtitle}</div>
+                    <div className="centering-div">
+                        <div className="search-landing__author">{`${search.User.firstName} ${search.User.lastName}`}</div>
                         <img className="search-landing__image" src={search.image} alt={search.title}/>
+                        <div onClick={()=>history.push(`/story/${search.id}`)} className="search-landing__title">{search.title}</div>
+                        <div className="search-landing__subtitle">{search.subtitle}</div>
                         <p className="search-landing__content">{search.content}</p>
                     </div>
                 </div>
