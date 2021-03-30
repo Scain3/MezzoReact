2918849bbc6709f0f5bbf0d4ee1e9c5d31ed13b6
salina kuo <salina.kuo97@gmail.com>
2021-03-30 17:17:34 -0400

    refactor feed

--- frontend/src/components/Feed/index.js
+++ frontend/src/components/Feed/index.js
@@ -2,17 +2,26 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 
-import StoryPreview from "./storyPreview";
-
-
 
 const Feed = () => {
 
     return (
             <>
                 <h1>This is the feed</h1>
+                
                 <div className="feed-container">
-                    {/* story preview */}
+                    <div className="previews-container">
+                        <div className="preview-container">
+                            <h3>title</h3>
+                            <p>subtitle</p>
+                            <div>
+                                {/* date */}
+                                {/* reading time */}
+                            </div>
+                        </div>
+
+                        <img></img>
+                    </div>
                 </div>
             </>
     )
