b2b43cb1f8a226a9e2fdafaaaa46568b88a30cd8
Safiya Cain <say.cain3@gmail.com>
2021-03-31 19:59:49 -0400

    add stories h3

--- frontend/src/components/Search/SearchPage.js
+++ frontend/src/components/Search/SearchPage.js
@@ -9,33 +9,36 @@ function SearchPage(){
 
     const searchInfo = useSelector(state => state.search.story);
     return(
-        <div>{
-            searchInfo && searchInfo.map((search, index) => (
-                <div className="search-landing__container" >
-                    <div className="centering-div">
-                        <h3 className="search-landing__author">{`${search.User.firstName} ${search.User.lastName}`}</h3>
-                        <p className="story-page-stats">
-							{`${parseDate(search.createdAt)}`}{" "}
-							<span className="story-page-stats-spacer">•</span>{" "}
-							{`${estimateTime(search.content)} min read`}
-							<span className="story-page-stats-spacer">
-                                <i className="fas fa-music"></i>
-							</span>{" "}
-                        </p>
-                        <img className="search-landing__image" src={search.image} alt={search.title}/>
-                        <div>
-                            <div onClick={()=>history.push(`/story/${search.id}`)} className="search-landing__title">{search.title}</div>
-                            <div className="search-landing__subtitle">{search.subtitle}</div>
-                            <div className="search-landing__content" onClick={()=>history.push(`/story/${search.id}`)} >Read More...</div>
+        <div>
+            <h3>STORIES</h3>
+            <div>{
+                searchInfo && searchInfo.map((search, index) => (
+                    <div className="search-landing__container" >
+                        <div className="centering-div">
+                            <h3 className="search-landing__author">{`${search.User.firstName} ${search.User.lastName}`}</h3>
+                            <p className="story-page-stats">
+                                {`${parseDate(search.createdAt)}`}{" "}
+                                <span className="story-page-stats-spacer">•</span>{" "}
+                                {`${estimateTime(search.content)} min read`}
+                                <span className="story-page-stats-spacer">
+                                    <i className="fas fa-music"></i>
+                                </span>{" "}
+                            </p>
+                            <img className="search-landing__image" src={search.image} alt={search.title}/>
+                            <div>
+                                <div onClick={()=>history.push(`/story/${search.id}`)} className="search-landing__title">{search.title}</div>
+                                <div className="search-landing__subtitle">{search.subtitle}</div>
+                                <div className="search-landing__content" onClick={()=>history.push(`/story/${search.id}`)} >Read More...</div>
+                            </div>
+                        </div>
+                        <div className="right-div">
+
                         </div>
                     </div>
-                    <div className="right-div">
+                ))
 
-                    </div>
-                </div>
-            ))
-
-            }</div>
+                }</div>
+        </div>
     )
 }
 
