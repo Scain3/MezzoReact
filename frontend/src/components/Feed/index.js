032d389645b8add7646dcc580a5e4c54a40b880c
salina kuo <salina.kuo97@gmail.com>
2021-03-31 16:01:02 -0400

    feed previews

--- frontend/src/components/Feed/index.js
+++ frontend/src/components/Feed/index.js
@@ -1,28 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-
+import { getTrendingStories } from "../../store/trending";
 
 
 const Feed = () => {
+    // temporary
+    const feedArticles = useSelector((state) => state.trending.stories)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        (async () => {
+            await dispatch(getTrendingStories());
+            return setLoaded(true);
+        })();
+    }, [dispatch]);
 
     return (
             <>
                 <h1>This is the feed</h1>
-                
-                <div className="feed-container">
-                    <div className="previews-container">
-                        <div className="preview-container">
-                            <h3>title</h3>
-                            <p>subtitle</p>
-                            <div>
-                                {/* date */}
-                                {/* reading time */}
-                            </div>
-                        </div>
 
-                        <img></img>
+                {articles.map((article) => (
+
+                    <div className="feed-container">
+                        <div className="feed-preview-container">
+                            <div className="feed-details">
+                                <p>{`${article.User.firstName} ${article.User.lastName}`}</p>
+                                <h3>{article.title}</h3>
+                                <p>{article.subtitle}</p>
+                                <div>
+                                    <p>{article.createdAt}</p>
+                                    <p>{article.content}</p>
+                                </div>
+                            </div>
+
+                            <img></img>
+                        </div>
                     </div>
-                </div>
+
+                ))}
             </>
     )
 }
