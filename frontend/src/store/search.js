16a833171763dcc287a10fe96d7f3897134d0643
Safiya Cain <say.cain3@gmail.com>
2021-04-06 18:57:29 -0400

    add search for author

--- frontend/src/store/search.js
+++ frontend/src/store/search.js
@@ -1,16 +1,23 @@
 import {fetch} from './csrf.js';
 
 const SEARCH_STORIES = 'SEARCH_STORIES';
+const SEARCH_AUTHORS = 'SEARCH_AUTHORS';
 
 const searchStories = (stories) => ({
     type: SEARCH_STORIES,
     payload: stories
 })
 
+const searchAuthors = (authors) => ({
+    type: SEARCH_AUTHORS,
+    payload: authors
+})
+
 export const fetchSearchInfo = (term) => async(dispatch) => {
     const res = await fetch(`/api/search?term=${encodeURIComponent(term)}`)
 
     dispatch(searchStories(res.data.stories));
+    dispatch(searchAuthors(res.data.authors));
 }
 
 const initialState = {story: null};
